06691cf7f0f1586c56d0f4333d6f10f9ebd5a2fc
Ahmed Mowafi <mwafid@gmail.com>
2023-10-05 13:43:16 +0300

    Add LowestPositiveInt logic

--- src/LowestPositiveInt.php
+++ src/LowestPositiveInt.php
@@ -1,8 +1,27 @@
 <?php
 
-namespace src;
+namespace App;
 
 class LowestPositiveInt
 {
+    public function find (array $numbers) : int
+    {
+        $numbers_status = array_fill(0, count($numbers) + 1, false);
 
+        foreach ($numbers as $number) {
+
+            if ( $number > 0 ) $numbers_status[$number] = true;
+
+        }
+
+        for ($i = 1; $i < count($numbers_status); $i++) {
+
+            if ($numbers_status[$i] === false) return $i;
+
+        }
+
+        return count($numbers_status);
+
+    }
 }
+
